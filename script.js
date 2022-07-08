8dd96fdf921302265a371ef06c1dd2ad971f7f1d
Weirdddude <weirdoproductions13@gmail.com>
2022-07-08 11:02:58 +0200

    Moved all funcs from HTML to JS

--- script.js
+++ script.js
@@ -16,3 +16,56 @@ function computerPlay(){
         compGuess = "scissors";
     }
 }
+//________________________________________________________________
+//Function maintaining the game
+function playGame(){
+    computerPlay();
+    playerGuess = String(prompt("Rock Paper or SCISSORS!!:").toLowerCase());
+    if (compGuess == playerGuess) {
+        console.log("Heck Yeah! " + playerGuess + " beats " + compGuess);
+        return 1;
+    }
+    else{
+        console.log("Oh no! " + compGuess + " beats " + playerGuess);
+        return 2;
+    }
+}
+
+//________________________________________________________________
+//Function to reset the game
+function reset(){
+    computerScore = 0;
+    playerScore = 0;
+}
+//________________________________________________________________
+//Function to play the game again
+function playAgain(){
+    if(computerScore>playerScore) alert("The computer won, oof!");
+    else alert("Hey! You Won!! WWOOOOOO!!!");
+    
+    askPlay = prompt("Do you want to play again? :")
+    if(askPlay == "Yes" || askPlay == "yes"){
+        playRounds();
+        reset();
+    }
+    else if (askPlay == "No" || askPlay == "no"){
+        alert("Okay, Bye")
+        reset();
+    }
+}
+//________________________________________________________________
+//Function for rounds implementation
+// function playRounds() {
+//     for(let round = 0; round < 5; round++){
+//         if(playGame() == 1 ){
+//             playerScore++;
+//             console.log("Playerscore: " + playerScore + " | Computerscore: " + computerScore);
+//         }
+//         else{
+//             computerScore++;
+//             console.log("Computerscore: " + computerScore + " | Playerscore: " + playerScore);
+//         }
+//     }
+//     playAgain();
+// }
+
